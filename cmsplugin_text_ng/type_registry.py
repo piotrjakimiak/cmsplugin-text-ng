6695afdadb45d08ae3010f7a481481397122d43f
Piotr Jakimiak <piotr.jakimiak@gmail.com>
2016-04-07 10:07:08 +0200

    Reversion support

--- cmsplugin_text_ng/type_registry.py
+++ cmsplugin_text_ng/type_registry.py
@@ -6,7 +6,9 @@ from cmsplugin_text_ng import exceptions
 _registry = {}
 
 def register_type(type_name, model_class):
-    from cmsplugin_text_ng.models import TextNGVariableBase
+    from cms.utils.helpers import reversion_register
+    from reversion.revisions import unregister
+    from cmsplugin_text_ng.models import TextNG, TextNGVariableBase
     if type_name in _registry:
         if _registry[type_name] == model_class:
             # already registered
@@ -24,6 +26,12 @@ def register_type(type_name, model_class):
     if not field.null:
         raise exceptions.InvalidType('"value" field of %s is not nullable' % model_class.__name__)
     _registry[type_name] = model_class
+    fields = [rel.get_accessor_name() for rel in TextNG._meta.get_all_related_objects() if issubclass(rel.related_model, TextNGVariableBase)]
+    try:
+        unregister(TextNG)
+    except KeyError:
+        pass
+    reversion_register(TextNG, follow=fields)
 
 
 def get_type(type_name):
